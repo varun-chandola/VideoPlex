aafc2376277628f4d01daebd2871085aa77b3863
Varun Chandola <139681679+varun-chandola@users.noreply.github.com>
2024-10-03 18:23:43 +0000

    Backend and DB initial setup

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js"
+
+dotenv.config({
+    path: "./.env"
+})
+
+connectDB().then(() => {
+    app.on("error", (error) => {
+        console.log("ERROR ON EXPRESS SERVER : ", error)
+    })
+    app.listen(process.env.PORT || 8000, () => console.log(`server running on ${process.env.PORT}`))
+})
+    .catch(err => console.log("MONGO DB CONNECTION FAILED : ", err))
