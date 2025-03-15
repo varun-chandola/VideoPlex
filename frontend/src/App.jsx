4f7698dc4af96a213f9b76ec4cdd316604d59dca
Varun Chandola <139681679+varun-chandola@users.noreply.github.com>
2025-03-15 03:28:42 +0000

    routes and home page setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,27 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from "axios"
-import toast, { Toaster } from 'react-hot-toast'
+import toast from 'react-hot-toast'
+import { BACKEND_URL } from '../../server/src/constants.js'
 const App = () => {
-  // username, password, email, fullName
   const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [email, setEmail] = useState('')
   const [fullName, setFullName] = useState('')
-  const [avatar, setAvatar] = useState('')
-  const [coverImage, setCoverImage] = useState('')
+  const [password, setPassword] = useState('')
   const [loading, setLoading] = useState(false)
-
 
   const navigate = useNavigate()
   const handleRegister = async (e) => {
     try {
       setLoading(true)
       e.preventDefault()
-      const formData = new FormData()
-      formData.append('username', username)
-      formData.append('password', password)
-      formData.append('email', email)
-      formData.append('fullName', fullName)
-      formData.append('avatar', avatar)
-      formData.append('coverImage', coverImage)
-
-      const response = await axios.post(`http://localhost:8000/api/v1/users/register`, formData, { withCredentials: true })
-      if (response.data.message == "Account Created Successfully") {
+      const response = await axios.post(`${BACKEND_URL}/users/register`, {
+        username,
+        password,
+        fullName
+      }, { withCredentials: true })
+      if (response.status(201)) {
         setLoading(false)
-        toast.success(response.data.message)
+        toast.success(response.data?.message)
         localStorage.setItem('token', response.data?.user?.token)
         navigate('/home')
       }
@@ -44,22 +36,9 @@ const App = () => {
   }
   return (
     <>
-      <div className='w-1/2 flex flex-col justify-center m-auto h-screen gap-2'>
-        <h1 className='flex items-center justify-center mb-4 font-bold text-2xl'>Register To VideoPlex</h1>
+      <div className='w-1/3 flex flex-col justify-center m-auto h-screen gap-2'>
+        <h1 className='flex items-center justify-center mb-4 font-bold text-2xl'>Register To StockPlex</h1>
         <form onSubmit={e => handleRegister(e)}>
-          <label className="input input-bordered flex items-center gap-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="h-4 w-4 opacity-70">
-              <path
-                d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-              <path
-                d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-            </svg>
-            <input type="text" required className="grow" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
-          </label>
           <label className="input input-bordered flex items-center gap-2">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -93,18 +72,15 @@ const App = () => {
                 d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
                 clipRule="evenodd" />
             </svg>
-            <input type="password" required className="grow" placeholder="**********" value={password} onChange={e => setPassword(e.target.value)} />
+            <input type="password" required className="grow" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
           </label>
-          <label>
-            Avatar
-            <input type="file" required className="file-input file-input-bordered w-full" onChange={e => setAvatar(e.target.files[0])} />
-          </label>
-          <label>
-            Cover Image
-            <input type="file" className="file-input file-input-bordered w-full" onChange={e => setCoverImage(e.target.files[0])} />
-          </label>
-          <button className="btn btn-info w-full mt-5">
-            {loading ? <span class="loading loading-spinner loading-md"></span> : `Register`}
+          <button className={`btn ${loading ? `bg-blue-800 hover:bg-blue-700` : `btn-info`} w-full mt-5`}>
+            {loading ?
+              <div className='flex items-center'>
+                <p className='text-l'>Registering...</p>
+                <span className="loading loading-spinner loading-sm"></span>
+              </div>
+              : `Register`}
           </button>
         </form>
         <p className='mt-3'>Already a user ? <Link className='text-blue-400 underline hover:cursor-pointer' to='/login'>Login</Link></p>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,31 +3,46 @@ import { createRoot } from 'react-dom/client'
 import { Toaster } from "react-hot-toast"
 import './index.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import App from './App.jsx'
-import Login from "./components/Login.jsx"
-import Home from './components/Home.jsx'
 import Context from './components/Context.jsx'
+import App from './App.jsx'
+import Login from './pages/Login.jsx'
+import Home from './pages/Home.jsx'
+import Explore from './pages/Explore.jsx'
+import Channel from "./pages/Channel.jsx"
 import History from './components/History.jsx'
 import Profile from "./components/Profile.jsx"
-import Subscriptions from './components/Subscriptions.jsx'
 import Likes from './components/Likes.jsx'
+import Playlist from "./pages/Playlist.jsx"
+import CreatorChannel from "./pages/CreatorChannel.jsx"
 import Video from "./components/Video.jsx"
+import Settings from './pages/Settings.jsx'
+import Layout from './components/Layout.jsx'
+import UpdateVideoInfo from './pages/UpdateVideoInfo.jsx'
+import Publish from "./pages/Publish.jsx"
 
 createRoot(document.getElementById('root')).render(
   <Context>
     <BrowserRouter>
       <Toaster />
       <Routes>
-        <Route path='/' element={<App />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/history' element={<History />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/subscriptions' element={<Subscriptions />} />
-        <Route path='/likes' element={<Likes />} />
-        <Route path='/video/:videoId' element={<Video />} />
+        <Route path='/' element={<App />} />
+
+        <Route element={<Layout />}>
+          <Route path='/home' element={<Home />} />
+          <Route path='/explore' element={<Explore />} />
+          <Route path='/:videoId/update' element={<UpdateVideoInfo />} />
+          <Route path='/you/channel' element={<Channel />} />
+          <Route path='/history' element={<History />} />
+          <Route path='/likes' element={<Likes />} />
+          <Route path='/playlist' element={<Playlist />} />
+          <Route path='/channel/:channelId' element={<CreatorChannel />} />
+          <Route path='/settings' element={<Settings />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/watch/:videoId' element={<Video />} />
+          <Route path='/publish' element={<Publish />} />
+        </Route>
       </Routes>
-    </BrowserRouter >
+    </BrowserRouter>
   </Context>
 );
-
