02a8982d85a43fdb7e3dca63e0cceea9068dd6d1
Varun Chandola <139681679+varun-chandola@users.noreply.github.com>
2025-03-15 09:49:31 +0530

    Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from "axios"
 import toast from 'react-hot-toast'
-import { BACKEND_URL } from '../../server/src/constants.js'
 const App = () => {
   const [username, setUsername] = useState('')
   const [fullName, setFullName] = useState('')
